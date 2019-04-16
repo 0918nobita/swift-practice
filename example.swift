8679d15f0f2d46025a8f8d1b24c8c969579d7756
0918nobita <nobita.0918@gmail.com>
2019-04-16 20:16:11 +0900

    :sparkles: protocol, struct を用いたサンプルを追加した

---
+++ example.swift
@@ -0,0 +1,28 @@
+class Ordered {
+  func precedes(other: Ordered) -> Bool {
+    fatalError("Implement me!")
+  }
+}
+
+class Number: Ordered {
+  var value: Double = 0
+
+  override func precedes(other: Ordered) -> Bool {
+    return value < (other as! Number).value  // 2
+  }
+}
+
+// Swift には抽象クラスがないので、
+// 上記 Ordered クラスのような「基底クラス」的なものを定義するのはイケてない
+// other as! Number でダウンキャストしてるのがよろしくない
+
+protocol Ordered2 {
+  func precedes(other: Self) -> Bool
+}
+
+struct Number2: Ordered2 {
+  var value: Double = 0
+  func precedes(other: Number2) -> Bool {
+    return value < other.value
+  }
+}
